7d949836fe0a5a2cf2e448c06454ebab93264876
Zehui-Lin <zehui-lin_t@outlook.com>
2019-11-02 16:03:53 +0800

    visual

--- CelebA/generate_visual_result.py
+++ CelebA/generate_visual_result.py
@@ -23,23 +23,43 @@ def creat_gif(result_path, gif_name, duration=0.2):
 
 creat_gif('./result', gif_name)
 
+
 #generate ground true image
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import glob
 import pathlib
-from dcgan import IMG_WIDTH
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+IMG_WIDTH = 128 #origin:178
+IMG_HEIGHT = 128 #origin:218
+sample_dir = './samples/'
+data_root = pathlib.Path(sample_dir)
+all_image_paths = list(data_root.glob('*'))
+all_image_paths = [str(path) for path in all_image_paths]
+
+def preprocess_image(image):
+  image = tf.image.decode_jpeg(image, channels=3)
+  image = tf.image.resize(image, [IMG_WIDTH, IMG_HEIGHT])
+  image /= 255.0  # normalize to [0,1] range
+
+  return image
+
+def load_and_preprocess_image(path):
+  image = tf.io.read_file(path)
+  return preprocess_image(image)
+
+
+path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
+image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+
+
 fig = plt.figure(figsize=(4, 4))
-data_dir = './sample'
-data_dir = pathlib.Path(data_dir)
-list_ds = tf.data.Dataset.list_files(str(data_dir/'*'))
-decoded_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 
-for i in range(16):
-    plt.subplot(4, 4, i + 1)
-    plt.imshow(train_images[i, :, :, :] * 127.5 + 127.5, cmap='gray')
-    plt.axis('off')
+for n, image in enumerate(image_ds.take(16)):
+  plt.subplot(4,4,n+1)
+  plt.imshow(image)
+  plt.axis('off')
+# plt.show()
 plt.savefig('ground_true_image')
-
-
 
 
